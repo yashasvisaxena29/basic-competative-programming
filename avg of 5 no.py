21881fb715233600f76bea55891fc3cdc7fa9d37
yashasvisaxena29 <yashasvi29saxena@gmail.com>
2025-08-24 21:23:18 +0530

    Add files via upload

---
+++ avg of 5 no.py
@@ -0,0 +1,7 @@
+A = float(input("Enter number A: "))
+B = float(input("Enter number B: "))
+C = float(input("Enter number C: "))
+D = float(input("Enter number D: "))
+E = float(input("Enter number E: "))
+average = (A + B + C + D + E) / 5
+print("The average of the 5 numbers is:", average)
